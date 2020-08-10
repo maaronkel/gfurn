ce3a3e954723ec4901a1e362e41ebeee6ff503db
maaronkel <maaronkel@protonmail.com>
2020-08-10 12:58:49 +1000

    Edit add / remove redirect links

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,13 +58,13 @@ class ItemsController < ApplicationController
   def add_to_cart
     id = params[:id].to_i
     session[:cart] << id unless session[:cart].include?(id)
-    redirect_to items_url
+    redirect_to cart_session_url
   end
 
   def remove_from_cart
     id = params[:id].to_i
     session[:cart].delete(id)
-    redirect_to items_url
+    redirect_to cart_session_url
   end
 
   def cart_session
